b8f22aee6d9848ca1793809fe99e6f49598be6bc
Andres Castillo <fabianboc960@hotmail.com>
2026-03-21 10:26:00 -0500

    feat: implement theme toggle functionality and update styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
+import { useEffect, useState } from "react";
 import TaskPage from "./pages/TaskPage";
+import type { Theme } from "./types/theme";
+
+const THEME_STORAGE_KEY = "tasks-manager:theme:v1";
 
 export default function App() {
+
+  const [theme, setTheme] = useState<Theme>(()=>{
+    const savedTheme = localStorage.getItem(THEME_STORAGE_KEY);
+    return savedTheme as Theme || "dark";
+  });
+
+  useEffect(()=>{
+    document.body.classList.remove("light-theme", "dark-theme");
+    document.body.classList.add(`${theme}-theme`);
+    localStorage.setItem(THEME_STORAGE_KEY, theme);
+  },[theme]);
+
+  function handleToggleTheme(): void {
+    setTheme( (prevTheme) => prevTheme === "light" ? "dark" : "light");
+  }
+  
   return (
     <main className="app-container">
       <section className="app-card">
-        <h1 className="app-title">Task Manager</h1>
+        <div className="app-header">
+          <h1 className="app-title">Task Manager</h1>
+          <button className="theme-button" onClick={handleToggleTheme}>
+            {theme === "light" ? "🌙 Dark Mode" : "🌞 Light Mode"}
+          </button>
+        </div>
         <TaskPage />
       </section>
     </main>
